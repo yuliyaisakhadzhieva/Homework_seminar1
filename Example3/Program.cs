6bc8d1a6c9da20732042eb230f0b5a3e2e7bf39b
Yuliya <yuliya_saveleva@list.ru>
2022-10-28 13:51:04 +0500

    Add Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int x = Convert.ToInt32 (Console.ReadLine());
+if (x % 2 ==0) Console.WriteLine (" Четное число");
+else Console.WriteLine (" Нечетное число");
+
