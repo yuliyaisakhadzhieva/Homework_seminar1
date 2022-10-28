d11b994f4b88001d31cf23910c332c84e2206f66
Yuliya <yuliya_saveleva@list.ru>
2022-10-28 12:36:29 +0500

    Add Example1

---
+++ Example1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите натурально число: ");
+int x = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите натурально число: ");
+int y = Convert.ToInt32(Console.ReadLine ());
+if (x > y) 
+    Console.Write (x + " Максимальное число ");
+else Console.Write (x + " Минимальное число ");
+if (x < y)
+    Console.Write (y + " Максимальное число ");
+else Console.Write (y + " Минимальное число ");
