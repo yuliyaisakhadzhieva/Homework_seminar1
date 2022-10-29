d180fb7b4c23f22bc19578eb79549754f8a930c9
Yuliya <yuliya_saveleva@list.ru>
2022-10-29 11:20:01 +0500

    Add Example4

---
+++ Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число");
+int x = Convert.ToInt32 (Console.ReadLine ());
+
+int i = 0;
+int y = i + 1;
+
+while (y < x)
+{
+    if (y % 2 == 0)
+    Console.Write (y + ", ");
+    y ++;
+}
